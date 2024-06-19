db127b7bfe40f2b84cd8438e8d95afb342731bb6
EmilioJanM <44309304+EmilioJanM@users.noreply.github.com>
2024-06-18 18:44:53 -0600

    Ink and tilemaps
    added some decoration like flowers to the map, there was a change in the character and added the Ink system for unity dialogues

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,35 +4,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] GameObject camera;
     public float _speed;
     private Rigidbody2D _rid;
     Vector3 _movement;
     Animator _animator;
 
+    [SerializeField] private float _normalScale;
+
+    
+
     void Start()
     {
+        camera = Camera.main.gameObject;
         _rid = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        camera.transform.position = new Vector3(transform.position.x, transform.position.y, -20);
 
         //We get the dot product of the X axis and the movement of the player to know to which side the player is going and set the side
         float dotDirection = Vector3.Dot(Vector3.Normalize(_movement), Vector3.right);
-        float currentScale = (dotDirection >= 0) ? 0.46f : -0.46f;
-        transform.localScale = new Vector3(currentScale, 0.46f, 0.46f);
+        float currentScale = (dotDirection >= 0) ? 0.24f : -0.24f;
+        transform.localScale = new Vector3(currentScale, 0.24f, 0.24f);
 
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _animator.SetBool("isRunning", false);
+        }
 
         //Change of the animation if there is movement
-        if(_movement.magnitude > 0)
+        if (_movement.magnitude > 0)
         {
             _animator.SetBool("isWalking", true);
+
+            if (Input.GetKeyDown(KeyCode.LeftShift)){
+                _animator.SetBool("isRunning", true);
+            }
         }
         else
         {
             _animator.SetBool("isWalking", false);
         }
+
+
     }
 
     private void FixedUpdate()
@@ -43,6 +61,11 @@ public class PlayerMovement : MonoBehaviour
 
         _movement = new Vector3(inputX * _speed, inputY * _speed);
         _movement *= Time.fixedDeltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _movement *= 1.5f;
+        }
 
         _rid.velocity = _movement;
     }
